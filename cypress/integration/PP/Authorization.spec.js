bf961ea11a2fb95691af2044c3982084554654dd
RomanChugunov <roman.chugunov@flatstack.com>
2020-12-04 04:48:59 -0600

    Added some tests
    Changed settings
    Added tests in "purchase sessions"
    Added tests in "authorization"

--- cypress/integration/PP/Authorization.spec.js
+++ cypress/integration/PP/Authorization.spec.js
@@ -2,26 +2,55 @@
 
 describe('authorization', function() {
 
-	const url = 'https://premiumparking.com'
+	const url = 'https://spa.release.premiumparking.com'
 	const email = 'roman.chugunov+a@flatstack.com'
-	const password = 'qqqwww'
+	const new_email = 'roman.chugunov+00003@flatstack.com'
+	const password = 'qqqaaa'
 
 
 	it('sign in', function() {
 
-	// Login execution
-	cy.visit(url)
-	cy.get('[data-cy=test-signin]').click().as('Click_Sign_In')
-	cy.wait(200)
-	cy.get('[data-cy=test-email-input]').as('Email').type(email)
-	cy.get('[data-cy=submit-btn]').as('Confirm').click()
-  	cy.get('[data-cy="test-password-input"]').as('Password').type(password)
-	cy.get('@Confirm').click()
+		// Login execution
+		cy.visit(url)
+		cy.get('[data-cy=test-signin]').click()
+		cy.wait(750)
+		cy.get('[data-cy=test-email-input]').type(email)
+		cy.wait(750)
+		cy.get('[data-cy=submit-btn]').as('Confirm').click()
+  		cy.get('[data-cy="test-password-input"]').as('Password').type(password)
+		cy.get('@Confirm').click()
+		cy.visit(url)
 
-	// Assertion that account screen opens
-	cy.url('pathname').should('contain', 'accounts')
+		// Assertion that email address is presented in the DOM
+  		cy.contains(email).should('be.visible')
+  	});
 
-	// Assertion taht email address is presented in the DOM
-  	cy.contains(email).should('be.visible')
-  });
-})	
+ 	it('sign_up', function() {
+
+		// Sign up execution
+		cy.visit(url)
+		cy.get('[data-cy=test-signin]').click()
+		cy.wait(1000)
+		cy.get('[data-cy=test-email-input]').type(new_email)
+		cy.wait(750)
+		cy.get('[data-cy=submit-btn]').click()
+		cy.get('[type="password"]').type(password)
+		cy.get('[data-cy=submit-btn]').click()
+		cy.visit(url)
+
+		// Assertion that email address is presented in the DOM
+		cy.contains(new_email).should('be.visible')
+	})
+
+	it('delete the account')
+		// Deleting the created account
+		cy.visit("https://pp-admin:premium@manage.release.premiumparking.com/profiles")
+		cy.get('[id=user_email').type("fs-admin@flatstack.com")
+		cy.get('[id=user_password').type("321premium")
+		cy.get('.btn').click()
+		cy.get('tbody > :nth-child(1) > :nth-child(1) > a').click()
+		cy.contains("Edit").click()
+		cy.contains("Archive").click()
+		cy.contains("Recover").should(be.visible)
+  })
+});
--- cypress/integration/PP/Purchase_session.js
+++ cypress/integration/PP/Purchase_session.js
@@ -1,40 +1,91 @@
+beforeEach(() => {
+  // Login execution
+  cy.visit(url)
+  cy.get('[data-cy=test-signin]').click()
+  cy.wait(750)
+  cy.get('[data-cy=test-email-input]').type(email)
+  cy.wait(750)
+  cy.get('[data-cy=submit-btn]').as('Confirm').click()
+  cy.get('[data-cy="test-password-input"]').as('Password').type(password)
+  cy.get('@Confirm').click()
+  cy.visit(url)
+})
+
+const url = 'https://spa.release.premiumparking.com'
+const email = 'roman.chugunov+a@flatstack.com'
+const new_email = 'roman.chugunov+00003@flatstack.com'
+const password = 'qqqaaa'
+const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+  window.HTMLInputElement.prototype,
+  'value'
+).set
+const changeRangeInputValue = $range => value => {
+  nativeInputValueSetter.call($range[0], value)
+  $range[0].dispatchEvent(new Event('change', { value, bubbles: true }))
+}
+
 describe('purchase_session', function() {
 
   it('purchase session with default vehicle and default card', function() {
-    cy.visit('https://spa.staging.premiumparking.com')
-
-  	cy.get('.react-autosuggest__input').type('P0123')
-
+    cy.visit('https://spa.release.premiumparking.com')
+    cy.get('.react-autosuggest__input').type('P0123')
+    cy.wait(200)
   	cy.get('.CustomButton__StyledButton-nw07qb-0').click()
-
   	cy.wait(1000)
-
-  	cy.get('.CustomButton__StyledButton-nw07qb-0').click()
-
   	cy.get('[data-testid=test-pay-to-park-now]').click()
-
-  	cy.get('[data-testid=test-input-range]').click()
-
+    cy.get('[data-testid=test-input-range]').then(input => changeRangeInputValue(input)(1))
   	cy.get('.Button__StyledButton-sc-1b506hy-0').click()
-
-  	cy.get('.commonStyledComponents__Form-sc-10ukbq6-0 > .ButtonedLink__StyledLink-sc-1vm1uho-0').click()
-
-    cy.get('.styledComponents__StyledInput-sc-1x2xdjn-2').type('roman.chugunov@flatstack.com')
-
-    cy.get('[data-testid=submit-btn]').click()
-
-    cy.wait(1000)
-
-    cy.get('.styledComponents__StyledInput-sc-1x2xdjn-2').type('qqqaaa')
-
-    cy.get('[data-testid=submit-btn]').click()
-
-  	cy.wait(1000)
-
+    cy.get('.commonStyledComponents__Form-sc-10ukbq6-0 > .ButtonedLink__StyledLink-sc-1vm1uho-0').click()
   	cy.get('[data-testid=test-pay-now]').click()
+    cy.get('.styles__Total-sc-1m9wmsg-10 > h4').should('contain', '10.40')
+  })
 
-    cy.get('.styles__RateInfo-sc-1m9wmsg-3').should('contain', 'TEST 12 Hrs')
+  it('purchase session rate and create new vehicle and card', function() {
+    cy.visit('https://spa.release.premiumparking.com/P123')
+    cy.contains("Rate menu").click()
+    cy.contains("3 min (special)").click()
+    cy.contains("Add new vehicle").click()
+    cy.get('[id="licensePlateNum"]').type("NEWVEHICLE")
+    cy.get('[data-testid=license-plate-state-select]').click()
+    cy.contains("Texas").click()
+    cy.get('[data-testid=make-select]').click()
+    cy.contains("Dodge").click()
+    cy.get('[data-testid=color-select]').click()
+    cy.contains("Black").click()
+    cy.contains("another card").click()
+    cy.get('[id="cardNumber"]').click()
+    cy.get('#cardNumber > .__PrivateStripeElement > iframe').its('0.contentDocument.body').then((body) => cy.wrap(body)).find('[name="cardnumber"]').type("6011111111111117")
+    cy.get('[id="cardExpiry"]').click()
+    cy.get('#cardExpiry > .__PrivateStripeElement > iframe').its('0.contentDocument.body').then((body) => cy.wrap(body)).find('[name="exp-date"]').type("1224")
+    cy.get('[id="cardCVC"]').click()
+    cy.get('#cardCVC > .__PrivateStripeElement > iframe').its('0.contentDocument.body').then((body) => cy.wrap(body)).find('[name="cvc"]').type("123")
+    cy.get('#zip').type("12345")
+    cy.get('.Checkbox__CustomCheckbox-fp6reb-2').click()
+    cy.get('[data-testid=test-pay-now]').click()
+    cy.get('.styles__Total-sc-1m9wmsg-10 > h4').should('contain', '2.40')
+  })
+  
+  it('delete last vehicle', function() {
+    cy.visit("https://pp-admin:premium@accounts.release.premiumparking.com/my/vehicles")
+    cy.contains("delete").click()
+    cy.contains("NEWVEHICLE").should('not.visible')
+    cy.contains("Vehicle has been successfully removed from your account!").should('be.visible')
+  })
 
-    cy.get('.styles__Total-sc-1m9wmsg-10 > h4').should('contain', '1.05')
+  it('delete last card', function() {
+    cy.visit("https://pp-admin:premium@accounts.release.premiumparking.com/my/payment_methods")
+    cy.contains("Delete").click()
+    cy.contains("1117").should('not.visible')
+    cy.contains("Credit card has been successfully removed from your account!").should('be.visible')
+  })
+
+  it('refill wallet', function(){
+    cy.visit("https://pp-admin:premium@accounts.release.premiumparking.com/my/payment_methods")
+    cy.contains("Refill Balance").click()
+    cy.get('#reload_wallet_form_amount').type("20")
+    cy.get('#reload_wallet_form_card_id').select('MasterCard •••• 4444, expires 12/23')
+    cy.get('.col-sm-12 > .btn').click()
+    cy.get('.payment-balance__num').should('contain', "$40.00")
+    cy.contains("Wallet has been successfully refilled!").should('be.visible')
   })
 })
